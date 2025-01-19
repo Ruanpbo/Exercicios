3f4831616dc1be8eda3b7d1e61afefca052578ce
Ruanpbo <ruanpbo@hotmail.com>
2025-01-19 17:22:37 -0300

    Iniciando repositorio

---
+++ Desafio1/app.js
@@ -0,0 +1,25 @@
+
+let diaDaSemana = prompt("Qual o dia da semana? ");
+
+if ( diaDaSemana == "Sábado" || diaDaSemana == "Domingo"){
+    alert("Bom fim de semana!")
+} else {
+    alert("Boa semana!")
+}
+
+let numero = prompt("Digite um numero");
+
+if (numero >= 0) {
+    alert(`O numero ${numero} é positivo`)
+} else {
+    alert(`O numero ${numero} é negativo`)
+}
+
+let pontos = prompt("Insira quantos tickets vc possui: ")
+
+if (pontos >= 100){
+    alert("Parabens, você venceu!")
+} else {
+    alert("Tente novamente para ganhar")
+}
+
