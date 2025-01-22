e11116c8104ae19a600907f757b3036a8870ce8e
Ruanpbo <ruanpbo@hotmail.com>
2025-01-22 17:22:30 -0300

    Desafio 05

---
+++ Desafio5/app.js
@@ -0,0 +1,11 @@
+let listaGenerica = [];
+
+let linguagensDeProgramação = ['JavaScript','C','C++', 'Kotlin','Python'];
+
+linguagensDeProgramação.push( 'Java', 'Ruby','GoLang');
+
+let amigos =['Kha', 'Andy', 'nick'];
+console.log(amigos[0]);
+console.log(amigos[1]);
+console.log(amigos[2]);
+console.log(amigos.length);
